3a2bb970ea361eb81dfb98eb3a1e203170123fef
Bencbabcock <bencbabcock01@gmail.com>
2025-01-24 15:34:47 -0500

    Update index.ts

--- ptcg-server/src/sets/set-diamond-and-pearl/index.ts
+++ ptcg-server/src/sets/set-diamond-and-pearl/index.ts
@@ -1,8 +1,10 @@
 import { Card } from '../../game/store/card/card';
+import { Meditite } from './meditite';
 import { PokedexHandy } from './pokedex-handy';
 import { SuperScoopUp } from './super-scoop-up';
 
 export const setDiamondAndPearl: Card[] = [
+  new Meditite(),
   new PokedexHandy(),
   new SuperScoopUp(),
 ];
