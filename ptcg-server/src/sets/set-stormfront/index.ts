9123c4227108dc95249ac20868e5c3133d3110a3
Bencbabcock <bencbabcock01@gmail.com>
2025-01-24 22:41:40 -0500

    Update index.ts

--- ptcg-server/src/sets/set-stormfront/index.ts
+++ ptcg-server/src/sets/set-stormfront/index.ts
@@ -1,10 +1,12 @@
 import { Card } from '../../game/store/card/card';
+import { Gastly } from './gastly';
 import { LuxuryBall } from './luxury-ball';
 import { PokeBlower } from './poke-blower';
 import { PokeDrawer } from './poke-drawer';
 import { Sableye } from './sableye';
 
 export const setStormfront: Card[] = [
+  new Gastly(),
   new LuxuryBall(),
   new PokeBlower(),
   new PokeDrawer(),
