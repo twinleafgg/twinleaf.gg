906eec763cb7810ed0ebb3821787d8f7208c02c7
Bencbabcock <bencbabcock01@gmail.com>
2025-01-24 21:15:10 -0500

    Update index.ts

--- ptcg-server/src/sets/set-legends-awakened/index.ts
+++ ptcg-server/src/sets/set-legends-awakened/index.ts
@@ -1,9 +1,10 @@
 import { Card } from '../../game/store/card/card';
+import { Lapras } from './lapras';
 import { UnownR } from './unown-r';
 import { Uxie } from './uxie';
 
 export const setLegendsAwakened: Card[] = [
-
+  new Lapras(),
   new UnownR(),
   new Uxie(),
 ];
