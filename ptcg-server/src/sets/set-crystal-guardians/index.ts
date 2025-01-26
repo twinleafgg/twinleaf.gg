c8669a0f5eee53daf8e8878f5ff5d14653a1319f
Bencbabcock <bencbabcock01@gmail.com>
2025-01-26 12:43:14 -0500

    Update index.ts

--- ptcg-server/src/sets/set-crystal-guardians/index.ts
+++ ptcg-server/src/sets/set-crystal-guardians/index.ts
@@ -1,6 +1,8 @@
 import { Card } from "../../game";
 import { Castaway } from "./castaway";
+import { Treecko } from "./treecko";
 
 export const setCrystalGuardians: Card[] = [
-  new Castaway()
+  new Castaway(),
+  new Treecko()
 ];
